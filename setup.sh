b2229182474805e539c288861eadfc45f39672ca
Dom Chester <dom.chester@me.com>
2022-04-08 02:07:19 +0100

    Added plugin to sync Atom plugins

--- setup.sh
+++ setup.sh
@@ -183,6 +183,7 @@
   ln -sf $resourceFolder/atom/config.cson ~/.atom
   ln -sf $resourceFolder/atom/init.coffee ~/.atom
   ln -sf $resourceFolder/atom/keymap.cson ~/.atom
+  ln -sf $resourceFolder/atom/packages.cson ~/.atom
   ln -sf $resourceFolder/atom/snippets.cson ~/.atom
   ln -sf $resourceFolder/atom/styles.less ~/.atom
 
