49a71ef2aea7b41c80ae8aee65ca7bf54bae7629
Dom Chester <dom.chester@me.com>
2022-04-13 10:42:28 +0100

    Check whether install, update, or relink

---
+++ tools/setup-macOS.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env zsh
+
+#-------------------------
+#------- Variables -------
+#-------------------------
+
+# Command Prefix
+  prefix="[Dot-Setup] "
+
+# Colours
+  RED="\033[1;31m"
+  GREEN="\033[1;32m"
+  YELLOW="\033[1;33m"
+  NOCOLOR="\033[0m"
+
+# Backup Folder Location
+  backupRoot=".dotBackups/"
+  backupFolder=$(date +"%Y-%m-%d_%H%M%S")
+
+# Resource Folder Location (where this script is)
+  resourceFolder=${0:a:h}"/"
+
+# Initialise Variables
+  installCode=0
+  needRestart=false
+
+#-------------------------
+#------- Functions -------
+#-------------------------
+
+# Check if a command exists, can be a binary in PATH or a shell
+  function has() {
+    type $1 &>/dev/null
+  }
+
+#-------------------------
+#---- Initialisation -----
+#-------------------------
+
+# Check if installing, updating, or re-linking
+  while [[ $installCode != 1 && $installCode != 2 && $installCode != 3 ]]; do
+    echo $YELLOW$prefix"Would you like to:"$NOCOLOR
+    echo $YELLOW$prefix"1 - Install"$NOCOLOR
+    echo $YELLOW$prefix"2 - Update"$NOCOLOR
+    echo $YELLOW$prefix"3 - Relink"$NOCOLOR
+
+    read "installCode?"$prefix"Please pick an option: "
+
+    if [[ $installCode != 1 && $installCode != 2 && $installCode != 3 ]]; then
+      echo $RED$prefix"Please pick an option from 1–3"
+    fi
+  done
